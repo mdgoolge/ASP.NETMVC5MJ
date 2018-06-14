58ab9160b17d1084c6ad210ede94a25dee6c27b4
a <a@a.com>
2018-06-14 08:46:29 +0800

    a

--- Mvc5Examples/Mvc5My/App_Start/RouteConfig.cs
+++ Mvc5Examples/Mvc5My/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mvc5My
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "MainIndex", id = UrlParameter.Optional }
             );
         }
     }
