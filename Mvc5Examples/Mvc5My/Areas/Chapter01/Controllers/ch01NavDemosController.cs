1ffadeb95298d174f0ea97b6c02f29ed64c09e3e
a <a@a.com>
2018-06-14 09:43:27 +0800

    sdf

---
+++ Mvc5Examples/Mvc5My/Areas/Chapter01/Controllers/ch01NavDemosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5My.Areas.Chapter01.Controllers
+{
+    public class ch01NavDemosController : Controller
+    {
+        // GET: Chapter01/ch01NavDemos
+        public ActionResult Index(string id)
+        {
+            return View(id);
+        }
+    }
+}
