e99b9fc9cb317edc7ccede41c195b5f0651911aa
a <a@a.com>
2018-06-20 19:59:39 +0800

    sdg

--- Mvc5Examples/Mvc5My/Areas/Chapter05/Controllers/ch05DemosController.cs
+++ Mvc5Examples/Mvc5My/Areas/Chapter05/Controllers/ch05DemosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mvc5My.Areas.Chapter05.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,12 +13,134 @@ namespace Mvc5My.Areas.Chapter05.Controllers
         public ActionResult Index(string id)
         {
             return View(id);
-        } 
-        
+        }
+
         //例1～例14、例21、例23～例24
         public ActionResult Index1(string id)
         {
             return PartialView(id);
         }
+
+        //例15
+        public ActionResult HttpGetDemo()
+        {
+            ViewBag.UserName = Request["text1"];
+            ViewBag.Age = Request["text2"];
+            return View();
+        }
+
+        //例16
+        public ActionResult HttpPostDemo()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                ViewBag.UserName = Request.Form["text1"];
+                ViewBag.Age = Request.Form["text2"];
+            }
+            else
+            {
+                ViewBag.UserName = "张三";
+                ViewBag.Age = 23;
+            }
+            return View();
+        }
+
+        //例17
+        public ActionResult HtmlBeginForm(MyClass1Model c1)
+        {
+            if (Request.HttpMethod == "GET")
+            {
+                c1 = new MyClass1Model { n1 = 3, n2 = 5 };
+                ViewBag.Result = "";
+            }
+            else
+            {
+                ViewBag.Result = string.Format("n1={0}, n2={1}", c1.n1, c1.n2);
+            }
+            return View(c1);
+        }
+
+        //例18
+        public ActionResult AjaxBeginForm(MyClass1Model c1)
+        {
+            if (Request.HttpMethod == "GET")
+            {
+                c1 = new MyClass1Model { n1 = 3, n2 = 5 };
+                ViewBag.Result = "";
+            }
+            else
+            {
+                ViewBag.Result = string.Format("n1={0}, n2={1}", c1.n1, c1.n2);
+            }
+            return PartialView(c1);
+        }
+
+
+        //例19
+        public ActionResult inputDemo()
+        {
+            ViewBag.Result = "等待提交数据。";
+            if (Request.HttpMethod == "POST")
+            {
+                if (this.ModelState.IsValid)
+                {
+                    ViewBag.Result = "数据已提交。";
+                }
+                else
+                {
+                    ViewBag.Result = "数据验证失败。";
+                }
+            }
+            return PartialView();
+        }
+
+
+        //例20
+        public ActionResult buttonDemo1(MyClass1Model c1)
+        {
+            if (Request.HttpMethod == "GET")
+            {
+                c1 = new MyClass1Model { n1 = 3, n2 = 5 };
+            }
+            else
+            {
+                string btn = Request["btn"];
+                ViewBag.result = "（服务器响应）你单击的按钮是：" + btn;
+            }
+            return PartialView(c1);
+        }
+        //例22
+        public ActionResult ButtonDemo3(MyClass1Model c1)
+        {
+            string result = "";
+            if (Request.HttpMethod == "GET")
+            {
+                c1 = new MyClass1Model();
+                c1.n1 = 3;
+                c1.n2 = 5;
+            }
+            else if (this.ModelState.IsValid)
+            {
+                string btn = Request["btn"];
+                switch (btn)
+                {
+                    case "求和":
+                        result = string.Format("{0} + {1} = {2}",
+                            c1.n1, c1.n2, c1.n1 + c1.n2);
+                        break;
+                    case "求积":
+                        result = string.Format("{0} × {1} = {2}",
+                            c1.n1, c1.n2, c1.n1 * c1.n2);
+                        break;
+                }
+            }
+            ViewBag.result = result;
+            return PartialView(c1);
+        }
+
+
+
+
+
     }
 }
---
+++ Mvc5Examples/Mvc5My/Areas/Chapter05/Models/MyClass2Model.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5My.Areas.Chapter05.Models
+{
+    public class MyClass2Model
+    {
+        [Display(Name = "性别")]
+        [Required]
+        public string Gender { get; set; }
+
+        /// <summary>体育活动</summary>
+        public Dictionary<string, bool> MySports { get; set; }
+
+        /// <summary>业余活动</summary>
+        public List<string> MyExtDoings { get; set; }
+
+        /// <summary>最喜欢的业余活动</summary>
+        public List<string> MyFavExtDoings { get; set; }
+
+        /// <summary>业余活动可选列表</summary>
+        public List<string> ExtDoingList { get; set; }
+
+        //设置默认选项
+        public MyClass2Model()
+        {
+            Gender = "女";
+            MySports = new Dictionary<string, bool>
+            {
+                {"足球", true},
+                {"篮球", false},
+                {"排球", false},
+                {"乒乓球", true},
+                {"羽毛球", false},
+            };
+            MyExtDoings = new List<string>
+            {
+                {"足球"}, {"排球"}, {"跳绳"}
+            };
+            MyFavExtDoings = new List<string>{ "跳绳" };
+            ExtDoingList = Enum.GetNames(typeof(DoingEnums)).ToList();
+        }
+    }
+    public enum DoingEnums {
+        足球, 篮球, 排球, 乒乓球, 羽毛球, 跳绳, 跑步, 钓鱼, 打猎, 游泳, 跳舞,
+        打牌, 下象棋, 下围棋, 下跳棋, 玩游戏, 逛商场, 网购, 看电影, 看电视,
+        玩手机, 其他
+    }
+}
