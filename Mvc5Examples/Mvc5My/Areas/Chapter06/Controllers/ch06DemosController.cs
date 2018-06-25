e8d191cdd643ab73275260e4c5908c30ac95d869
a <a@a.com>
2018-06-25 16:40:32 +0800

    a

---
+++ Mvc5Examples/Mvc5My/Areas/Chapter06/Controllers/ch06DemosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5My.Areas.Chapter06.Controllers
+{
+    public class ch06DemosController : Controller
+    {
+        //ch06Index.cshtml调用的操作方法
+        public ActionResult Index(string id)
+        {
+            return View(id);
+        } 
+        
+        //本章示例调用的操作方法
+        public ActionResult Index1(string id)
+        {
+            return PartialView(id);
+        }
+    }
+}
