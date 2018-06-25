db615945bedb164a0769d4bc2022805b6e7bff49
mdgoogle <gengtiwin@hotmail.com>
2018-06-25 17:08:24 +0800

    okk

---
+++ Mvc5Examples/Mvc5My/Areas/Chapter07/Controllers/ch07DemosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5My.Areas.Chapter07.Controllers
+{
+    public class ch07DemosController : Controller
+    {
+        public ActionResult Index(string id)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(id);
+            }
+            else
+            {
+                return View(id);
+            }
+        }
+    }
+}
