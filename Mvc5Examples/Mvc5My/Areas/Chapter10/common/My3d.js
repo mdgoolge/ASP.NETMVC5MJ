1e46f0e08746001be2b0f6c4018a03de8023b316
mdgoogle <gengtiwin@hotmail.com>
2018-06-29 16:35:07 +0800

    s

---
+++ Mvc5Examples/Mvc5My/Areas/Chapter10/common/My3d.js
@@ -0,0 +1,26 @@
+﻿var My3d = {};
+
+My3d.Width = function(){
+    /// <summary>获取WebGL容器的宽度</summary>
+    return window.innerWidth;
+};
+
+My3d.Height = function () {
+    /// <summary>获取WebGL容器的高度</summary>
+    return window.innerHeight;
+};
+
+My3d.aspect = function (){
+    /// <summary>获取WebGL容器的纵横比</summary>
+    return window.innerWidth / window.innerHeight;
+};
+
+My3d.HalfWidth = function () {
+    /// <summary>WebGL容器的一半高度</summary>
+    return window.innerWidth / 2;
+};
+
+My3d.HalfHeight = function () {
+    /// <summary>WebGL容器的一半高度</summary>
+    return window.innerHeight / 2;
+};
